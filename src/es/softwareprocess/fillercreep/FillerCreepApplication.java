ba48eaa19ae4dd3296150ee9558b53ca4726a5ae
Abram Hindle <abram.hindle@ualberta.ca>
2012-01-25 16:38:47 -0700

    Reformatting

--- src/es/softwareprocess/fillercreep/FillerCreepApplication.java
+++ src/es/softwareprocess/fillercreep/FillerCreepApplication.java
@@ -3,26 +3,28 @@ package es.softwareprocess.fillercreep;
 import android.app.Application;
 
 public class FillerCreepApplication extends Application {
-	transient private static FillerCreep fillerCreep = null;
-	static FillerCreep getFillerCreep() {		
-		if (fillerCreep == null) {
-			fillerCreep = new FillerCreep();
-		}
-		return fillerCreep;
-	}
-	
+    // Singleton
+    transient private static FillerCreep fillerCreep = null;
+
+    static FillerCreep getFillerCreep() {
+        if (fillerCreep == null) {
+            fillerCreep = new FillerCreep();
+        }
+        return fillerCreep;
+    }
+
+    // Singleton
+    transient private static GameController gameController = null;
+
+    public static GameController getGameController() {
+        if (gameController == null) {
+            gameController = new GameController(getFillerCreep());
+        }
+        return gameController;
+    }
+
     @Override
     public void onCreate() {
         super.onCreate();
     }
-
-    transient private static GameController gameController = null;
-	public static GameController getGameController() {
-		if (gameController == null) {
-			gameController = new GameController(getFillerCreep());
-		}
-		return gameController;
-	}
-
-	
 }
